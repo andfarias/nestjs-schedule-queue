51dbccf6675975b774143e76b09918b84afaba68
Anderson Farias <andersonsantofarias@outlook.com>
2022-06-04 18:21:26 -0300

    build: Add Docker

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -1,15 +1,35 @@
 import { InjectModel } from '@nestjs/sequelize';
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Tweet } from '../entities/tweet.entity';
 import { Interval } from '@nestjs/schedule';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class TweetsCountService {
+  private limit: number = 10;
+
   constructor(
     @InjectModel(Tweet)
     private tweetModel: typeof Tweet,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
 
   @Interval(5000)
-  async countTweets() {}
+  async countTweets() {
+    let offset = await this.cacheManager.get<number>('tweet-offset');
+    offset = offset === undefined ? 0 : offset;
+
+    const tweets = await this.tweetModel.findAll({
+      offset: offset,
+      limit: this.limit,
+    });
+
+    if (tweets.length === this.limit) {
+      offset + this.limit;
+      this.cacheManager.set('tweet-offset', offset + this.limit, {
+        ttl: 1 * 60 * 10,
+      });
+    }
+  }
 }
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
@@ -6,7 +6,7 @@ import { Tweet } from './entities/tweet.entity';
 import { TweetsCountService } from './tweets-count/tweets-count.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Tweet])],
+  imports: [CacheModule.register(), SequelizeModule.forFeature([Tweet])],
   controllers: [TweetsController],
   providers: [TweetsService, TweetsCountService],
 })
